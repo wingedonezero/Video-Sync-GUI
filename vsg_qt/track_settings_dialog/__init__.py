520afc1e988d055814c2e79985925bee64f6c388
wingedonezero <nomail@phantom.com>
2025-09-06 18:49:24 -0400

    fix

--- vsg_qt/track_settings_dialog/__init__.py
+++ vsg_qt/track_settings_dialog/__init__.py
@@ -1,7 +1,3 @@
-from .dialog import TrackSettingsDialog
-__all__ = ["TrackSettingsDialog"]
-# Re-export to preserve existing imports:
-# from vsg_qt.track_settings_dialog import TrackSettingsDialog
+# vsg_qt/track_settings_dialog/__init__.py
 from .ui import TrackSettingsDialog
-
 __all__ = ["TrackSettingsDialog"]
